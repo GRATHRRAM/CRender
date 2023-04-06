de14c4a573839e1b0acbe5d6e8bb7c8bc1b15211
Script_guy <101575465+scriptguy1@users.noreply.github.com>
2023-04-06 13:51:01 +0200

    Add files via upload

---
+++ examples/movingRect.c
@@ -0,0 +1,56 @@
+#include "CRender.c"
+//CRrender 0.35v
+int main(void)
+{
+    CR_RENDER RND;
+    CR_RECT RCT;
+    RCT.look = '@';
+    RCT.x = 2;
+    RCT.y = 2;
+    RCT.height = 3;
+    RCT.width = 2;
+
+
+    RND.RESOLUTION_X = 20;
+    RND.RESOLUTION_Y = 20;
+    CR_RENDER_ALOC(&RND);
+
+    printf("Press -> a to move left\nPress -> d to move right\nPress -> w to move up\nPress -> s to move down\nPress -> x to exit\nPress -> enter (two times) to start\n");
+    getchar();
+
+    while (1)
+    {
+        char input = CR_Int2Char(CR_GetInput());
+        system("clear");//on windows system("cls");
+
+        if(input == 'w')
+        {
+            RCT.y -= 1;
+        }
+        if(input == 's')
+        {
+            RCT.y += 1;
+        }
+        if(input == 'a')
+        {
+            RCT.x -= 1;
+        }
+        if(input == 'd')
+        {
+            RCT.x += 1;
+        }
+        if(input == 'x')
+        {
+            break;
+        }
+
+        CR_RENDER_FILL_ALL(&RND,'#');
+        for(int gc = 0; gc < RND.RESOLUTION_X * RND.RESOLUTION_Y;gc++)
+        {
+            CR_RENDER_SET_DIRECT_COLOR_of_PIXEL(&RND,gc,rand()%6+2);
+        }
+        CR_Rect2Render(&RND,RCT);
+        CR_RENDER_PRINT(RND);
+    }
+    return 0;
+}
